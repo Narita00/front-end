b60bd6c60a1615420854bf9a29591150c9d3e77d
narita <narita@example.com>
2019-05-01 13:35:08 +0900

    Finish Responsive Navigation Bar Tutorial

--- navigation/js/navSlide.js
+++ navigation/js/navSlide.js
@@ -1,4 +1,28 @@
 const navSlide = ()=>{
-    const burger = document.querySelector('.burger')
-    const nav = document.querySelector('.nav-links')
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+    burger.addEventListener('click',()=>{
+        //Toggle Nav
+        nav.classList.toggle('nav-active');
+
+        //Animate links
+        navLinks.forEach((link,index) => {
+            if(link.style.animation){
+                //To close
+                link.style.animation = '';
+                console.log('reset');
+            }else{
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7+0.3}s`;
+                console.log(link.style.animation);
+            }
+        });
+
+        burger.classList.toggle('toggle');
+
+    });
+
 }   
+
+navSlide();

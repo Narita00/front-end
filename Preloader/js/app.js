ee8a7173f47c41bde639aa7f3d38e31f0ddacd93
narita <narita@example.com>
2019-05-05 21:40:27 +0900

    Finish preloader

---
+++ Preloader/js/app.js
@@ -0,0 +1,7 @@
+//Need to make a function that get rid of the preloder
+
+window.addEventListener("load",()=>{
+    const preload = document.querySelector(".preload");
+
+    preload.classList.add("preload-finish");
+});
